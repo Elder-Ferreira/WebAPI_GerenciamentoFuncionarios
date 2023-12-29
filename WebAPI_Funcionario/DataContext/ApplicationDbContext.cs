12ea88bbd77ce5696247781fd488e30c2d5e53fc
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-12-29 12:43:45 -0300

    Configurando Conexão com banco de dados

---
+++ WebAPI_Funcionario/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Funcionario.Models;
+
+namespace WebAPI_Funcionario.DataContext
+{
+    public class ApplicationDbContext : DbContext 
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
